896321a7dad57ade26bce0a89ddde0d98dbae741
Gouthami Matavalam <gouthami.matavalam@worldline.com>
2024-03-07 23:57:22 +0530

    inital commit

---
+++ src/main/java/com/assignment/globaltransfersystem/GlobalTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.globaltransfersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTransferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTransferSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/globaltransfersystem/GlobalTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.globaltransfersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
